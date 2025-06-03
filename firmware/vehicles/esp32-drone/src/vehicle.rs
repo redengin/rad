982085011f71c26f081bac4731716c961026f2bf
redengin <sholstei@yahoo.com>
2025-06-03 01:20:00 -0400

    in work

--- firmware/vehicles/esp32-drone/src/vehicle.rs
+++ firmware/vehicles/esp32-drone/src/vehicle.rs
@@ -3,6 +3,7 @@
 ///=============================================================================
 use esp_hal::uart;
 
+// TODO use a rad_drone gps driver's data
 pub fn gps_uart_config() -> uart::Config {
     uart::Config::default()
     // the following show the default configuration and the method to modify
@@ -14,6 +15,7 @@ pub fn gps_uart_config() -> uart::Config {
     // .with_rx_timeout(10)
 }
 
+// TODO use a rad_drone imu driver's data
 pub fn imu_spi_config() -> esp_hal::spi::master::Config {
     esp_hal::spi::master::Config::default()
         .with_frequency(esp_hal::time::RateExtU32::kHz(100))
@@ -23,3 +25,53 @@ pub fn imu_spi_config() -> esp_hal::spi::master::Config {
 // pub fn esc_pwm_freq -> esp_hal::time::Rate {
 //     esp_hal::time::RateExtU32::Hz(50)
 // }
+
+pub struct Esp32Drone {
+    gps: esp_hal::uart::Uart<'static, esp_hal::Async>,
+    // imu: esp_hal::spi::master::SpiDma<'static, esp_hal::Async>
+}
+
+use esp_hal::gpio::interconnect::{PeripheralInput, PeripheralOutput};
+use esp_hal::peripheral::Peripheral;
+// use esp_hal::uart;
+use esp_hal::spi;
+
+impl Esp32Drone {
+    pub fn new(
+        gps_uart: impl Peripheral<P = impl uart::Instance> + 'static,
+        gps_tx_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        gps_rx_pin: impl Peripheral<P = impl PeripheralInput> + 'static,
+        imu_spi: impl Peripheral<P = impl spi::master::PeripheralInstance> + 'static,
+        // imu_dma: impl Peripheral<P = spi::master::SpiDma<'a, esp_hal::Async>::CH> + 'static,
+        // imu_dma: impl Peripheral<P = spi::master::SpiDma + 'static,
+        imu_sck: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        imu_mosi: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        imu_miso: impl Peripheral<P = impl PeripheralInput> + 'static,
+    ) -> Self {
+        // configure GPS hardware interface
+        let gps_uart = uart::Uart::new(gps_uart, gps_uart_config())
+            .unwrap()
+            .with_tx(gps_tx_pin)
+            .with_rx(gps_rx_pin)
+            .into_async();
+        // configure IMU hardware interface
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = esp_hal::dma_buffers!(32000);
+        let dma_rx_buf = esp_hal::dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = esp_hal::dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+        let mut imu =
+            esp_hal::spi::master::Spi::new(imu_spi, imu_spi_config())
+                .unwrap()
+                .with_sck(imu_sck) // IMU SPI-CLK
+                .with_mosi(imu_mosi) // IMU SPI-MOSI
+                .with_miso(imu_miso) // IMU SPI-MISO
+                // // .with_cs(peripherals.GPIO5)  // IMU SPI-CS
+                // .with_dma(imu_dma)
+                // .with_buffers(dma_rx_buf, dma_tx_buf)
+                .into_async();
+
+        Esp32Drone {
+            gps: gps_uart,
+            // imu: imu,
+         }
+    }
+}
