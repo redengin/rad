03e58062bd8b1b4c0495ec3ef0e5dc094bc9f612
redengin <sholstei@yahoo.com>
2025-06-04 00:59:41 -0400

    in work

--- firmware/vehicles/esp32-drone/src/bin/main.rs
+++ firmware/vehicles/esp32-drone/src/bin/main.rs
@@ -1,9 +1,5 @@
 #![no_std]
 #![no_main]
-use esp_hal::peripheral;
-// use re-exported dependencies
-use rad_drone::log;
-// use rad_drone::time::{Duration, Timer};
 
 // provide no_std support
 use esp_backtrace as _; // implements panic
@@ -12,17 +8,20 @@ use esp_backtrace as _; // implements panic
 pub extern "C" fn custom_halt() {
     esp_hal::reset::software_reset();
 }
-// provide required alloc implementation
+// provide esp_hal required alloc implementation
 use esp_alloc as _;
 
-use vehicle::{self, Esp32Drone};
+// use re-exported dependencies
+use rad_drone::log;
+
+// use local vehicle implementation
+use vehicle::Esp32Drone;
 
 #[esp_hal_embassy::main]
 async fn main(_spawner: embassy_executor::Spawner) {
-    // initialize SoC
-    let peripherals = esp_hal::init(
-        esp_hal::Config::default(), // .with_cpu_clock(esp_hal::clock::CpuClock::max())
-    );
+    // initialize SoC (with max compute - aka cpu_clock frequency)
+    let peripherals =
+        esp_hal::init(esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::max()));
 
     // initialize embassy scheduler
     #[cfg(feature = "esp32")]
@@ -32,8 +31,7 @@ async fn main(_spawner: embassy_executor::Spawner) {
     }
     #[cfg(not(feature = "esp32"))]
     {
-        use esp_hal::timer::systimer::SystemTimer;
-        let systimer = SystemTimer::new(peripherals.SYSTIMER);
+        let systimer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
         esp_hal_embassy::init(systimer.alarm0);
     }
 
@@ -46,11 +44,12 @@ async fn main(_spawner: embassy_executor::Spawner) {
     // create the vehicle
     let vehicle = Esp32Drone::new(
         // GPS interface
-        peripherals.UART0,  // GPS UART
-        peripherals.GPIO1,  // GPS Tx Pin
-        peripherals.GPIO3,  // GPS Tx Pin
+        peripherals.UART0, // GPS UART
+        peripherals.GPIO1, // GPS Tx Pin
+        peripherals.GPIO3, // GPS Tx Pin
         // IMU interface
-        peripherals.SPI2, peripherals.DMA_SPI2,
+        peripherals.SPI2,
+        peripherals.DMA_SPI2,
         peripherals.GPIO14, // IMU SPI CLK
         peripherals.GPIO13, // IMU SPI MOSI
         peripherals.GPIO12, // IMU SPI MISO
@@ -58,12 +57,12 @@ async fn main(_spawner: embassy_executor::Spawner) {
         // FIXME
         peripherals.MCPWM0,
         peripherals.MCPWM1,
-        peripherals.GPIO8,      // Motor A ESC pin 
-        peripherals.GPIO9,      // Motor B ESC pin 
-        peripherals.GPIO10,     // Motor C ESC pin 
-        peripherals.GPIO11,     // Motor D ESC pin 
+        peripherals.GPIO8,  // Motor A ESC pin
+        peripherals.GPIO9,  // Motor B ESC pin
+        peripherals.GPIO10, // Motor C ESC pin
+        peripherals.GPIO11, // Motor D ESC pin
     );
 
-    // // start the rad_drone tasks
+    // start the rad_drone tasks
     // rad_drone::start(spawner, vehicle);
 }
--- firmware/vehicles/esp32-drone/src/vehicle.rs
+++ firmware/vehicles/esp32-drone/src/vehicle.rs
@@ -1,12 +1,11 @@
 #![no_std]
 //! Hardware Configuration for the Vehicle
 ///=============================================================================
-use esp_hal::dma::DmaChannelFor;
-use esp_hal::gpio::interconnect::{PeripheralInput, PeripheralOutput};
-use esp_hal::mcpwm;
+use esp_hal::{uart, spi, mcpwm};
+use esp_hal::Async;
+use esp_hal::{dma, dma_buffers};
 use esp_hal::peripheral::Peripheral;
-use esp_hal::spi;
-use esp_hal::uart;
+use esp_hal::gpio::interconnect::{PeripheralInput, PeripheralOutput};
 
 // TODO use a rad_drone gps driver's data
 pub fn gps_uart_config() -> uart::Config {
@@ -21,26 +20,26 @@ pub fn gps_uart_config() -> uart::Config {
     // .with_rx_timeout(10)
 }
 
-pub fn imu_spi_config() -> esp_hal::spi::master::Config {
+pub fn imu_spi_config() -> spi::master::Config {
     // TODO use a rad_drone imu driver's config values
-    esp_hal::spi::master::Config::default()
+    spi::master::Config::default()
         .with_frequency(esp_hal::time::RateExtU32::kHz(100))
-        .with_mode(esp_hal::spi::Mode::_0)
+        .with_mode(spi::Mode::_0)
 }
 
-// TODO use rad_drone esc driver's config value
-const ESC_PWM_HZ: u32 = 50;
-// TODO use rad_drone esc driver's config value
-const ESC_MAX_DUTY_CYCLE: u16 = 100;
-/// determines rate of the pwm master clock
-///     (this * ESC_PWM_HZ) = master clock frequency
-///     (lower pwm master clock frequency is expected to conserve power)
-const ESC_PWM_MASTER_OVERSAMPLING_SCALAR: u32 = 1; // Nyquist = 2
+// // TODO use rad_drone esc driver's config value
+// const ESC_PWM_HZ: u32 = 50;
+// // TODO use rad_drone esc driver's config value
+// const ESC_MAX_DUTY_CYCLE: u16 = 100;
+// /// determines rate of the pwm master clock
+// ///     (this * ESC_PWM_HZ) = master clock frequency
+// ///     (lower pwm master clock frequency is expected to conserve power)
+// const ESC_PWM_MASTER_OVERSAMPLING_SCALAR: u32 = 1; // Nyquist = 2
 
 /// rad_drone::Vehicle
 pub struct Esp32Drone {
-    gps: uart::Uart<'static, esp_hal::Async>,
-    imu: spi::master::SpiDmaBus<'static, esp_hal::Async>,
+    gps: uart::Uart<'static, Async>,
+    imu: spi::master::SpiDmaBus<'static, Async>,
     // FIXME what is a PwmPin
     // esc_a_pwm: dyn mcpwm::PwmPeripheral,
 }
@@ -58,15 +57,15 @@ impl Esp32Drone {
         imu_mosi: impl Peripheral<P = impl PeripheralOutput> + 'static,
         imu_miso: impl Peripheral<P = impl PeripheralInput> + 'static,
         // motors
-        pwm_a: impl Peripheral<P = PWMA> + 'static,
-        pwm_b: impl Peripheral<P = PWMB> + 'static,
-        esc_a_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
-        esc_b_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
-        esc_c_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
-        esc_d_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        _pwm_a: impl Peripheral<P = PWMA> + 'static,
+        _pwm_b: impl Peripheral<P = PWMB> + 'static,
+        _esc_a_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        _esc_b_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        _esc_c_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
+        _esc_d_pin: impl Peripheral<P = impl PeripheralOutput> + 'static,
     ) -> Self
     where
-        SpiDmaCh: DmaChannelFor<spi::AnySpi>,
+        SpiDmaCh: dma::DmaChannelFor<spi::AnySpi>,
         PWMA: mcpwm::PwmPeripheral,
         PWMB: mcpwm::PwmPeripheral,
     {
@@ -79,10 +78,10 @@ impl Esp32Drone {
 
         // configure IMU hardware interface
         // create DMA buffers
-        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = esp_hal::dma_buffers!(32000);
-        let dma_rx_buf = esp_hal::dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
-        let dma_tx_buf = esp_hal::dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
-        let imu = esp_hal::spi::master::Spi::new(imu_spi, imu_spi_config())
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+        let dma_rx_buf = dma::DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = dma::DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+        let imu = spi::master::Spi::new(imu_spi, imu_spi_config())
             .unwrap()
             .with_sck(imu_sck) // IMU SPI-CLK
             .with_mosi(imu_mosi) // IMU SPI-MOSI
@@ -93,41 +92,41 @@ impl Esp32Drone {
             .into_async();
 
         // configure ESC hardware interfaces
-        let pwm_clock_cfg = mcpwm::PeripheralClockConfig::with_frequency(
-            esp_hal::time::RateExtU32::Hz(ESC_PWM_HZ * ESC_PWM_MASTER_OVERSAMPLING_SCALAR),
-        )
-        .unwrap();
-        let timer_clock_cfg = pwm_clock_cfg
-            .timer_clock_with_frequency(
-                ESC_MAX_DUTY_CYCLE, // output duty cycle range [0..ESC_MAX_DUTY_CYCLE]
-                mcpwm::timer::PwmWorkingMode::Increase,
-                esp_hal::time::RateExtU32::Hz(ESC_PWM_HZ),
-            )
-            .unwrap();
-        // create 3 pwm controllers on first mcpwm device
-        let mut mcpwmA = mcpwm::McPwm::new(pwm_a, pwm_clock_cfg);
-        // configure timer0 (default for all operator channels)
-        mcpwmA.timer0.start(timer_clock_cfg);
-        let mut esc_a_pwm = mcpwmA.operator0.with_pin_a(
-            esc_a_pin, // MOTOR-A speed controller pin
-            esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
-        );
-        let mut esc_b_pwm = mcpwmA.operator1.with_pin_a(
-            esc_b_pin, // MOTOR-B speed controller pin
-            esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
-        );
-        let mut esc_c_pwm = mcpwmA.operator2.with_pin_a(
-            esc_c_pin, // MOTOR-B speed controller pin
-            esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
-        );
-        // create 1 pwm controllers on second mcpwm device
-        let mut mcpwmB = mcpwm::McPwm::new(pwm_b, pwm_clock_cfg);
-        // configure timer0 (default for all operator channels)
-        mcpwmB.timer0.start(timer_clock_cfg);
-        let mut esc_d_pwm = mcpwmB.operator0.with_pin_a(
-            esc_d_pin, // MOTOR-A speed controller pin
-            esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
-        );
+        // let pwm_clock_cfg = mcpwm::PeripheralClockConfig::with_frequency(
+        //     esp_hal::time::RateExtU32::Hz(ESC_PWM_HZ * ESC_PWM_MASTER_OVERSAMPLING_SCALAR),
+        // )
+        // .unwrap();
+        // let timer_clock_cfg = pwm_clock_cfg
+        //     .timer_clock_with_frequency(
+        //         ESC_MAX_DUTY_CYCLE, // output duty cycle range [0..ESC_MAX_DUTY_CYCLE]
+        //         mcpwm::timer::PwmWorkingMode::Increase,
+        //         esp_hal::time::RateExtU32::Hz(ESC_PWM_HZ),
+        //     )
+        //     .unwrap();
+        // // create 3 pwm controllers on first mcpwm device
+        // let mut mcpwm_a = mcpwm::McPwm::new(pwm_a, pwm_clock_cfg);
+        // // configure timer0 (default for all operator channels)
+        // mcpwm_a.timer0.start(timer_clock_cfg);
+        // let mut esc_a_pwm = mcpwm_a.operator0.with_pin_a(
+        //     esc_a_pin, // MOTOR-A speed controller pin
+        //     mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+        // );
+        // let mut esc_b_pwm = mcpwm_a.operator1.with_pin_a(
+        //     esc_b_pin, // MOTOR-B speed controller pin
+        //     mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+        // );
+        // let mut esc_c_pwm = mcpwm_a.operator2.with_pin_a(
+        //     esc_c_pin, // MOTOR-B speed controller pin
+        //     mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+        // );
+        // // create 1 pwm controllers on second mcpwm device
+        // let mut mcpwm_b = mcpwm::McPwm::new(pwm_b, pwm_clock_cfg);
+        // // configure timer0 (default for all operator channels)
+        // mcpwm_b.timer0.start(timer_clock_cfg);
+        // let mut esc_d_pwm = mcpwm_b.operator0.with_pin_a(
+        //     esc_d_pin, // MOTOR-A speed controller pin
+        //     mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+        // );
 
         Esp32Drone {
             gps: gps_uart,
